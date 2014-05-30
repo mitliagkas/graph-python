6f95d29581354489fe518887878d09df8d91f9a9
Ioannis Mitliagkas <ioannis@utexas.edu>
2014-05-30 17:22:59 -0500

    First graph reader: twitter

---
+++ graph.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+"""Create graph representations from files etc."""
+import csv
+import scipy as sp
+from scipy import sparse
+
+def gettwittergraph():
+    """
+        Reads the edge list from the twitter file
+        http://snap.stanford.edu/data/egonets-Twitter.html
+        and creates a sparse directed edge matrix, A.
+
+        eg. if the file contains an line "a b" (a follows b),
+        then A[b,a]==1,
+        ow A[b,a]==0
+    :returns : A scipy sparse matrix (dok) containing all edges.
+    :rtype : scipy.sparse.dok_matrix, dict, dict
+    """
+    filename='/var/datasets/twitter/twitter_combined.txt'
+    reader = csv.reader(open(filename), delimiter=' ')
+
+    MAXINDEX=1e7
+
+    A=sparse.dok_matrix((int(MAXINDEX), int(MAXINDEX)))
+    indegree=dict()
+    outdegree=dict()
+
+    for entry in reader:
+        src=int(entry[0])
+        dst=int(entry[1])
+        if src>MAXINDEX or dst>MAXINDEX:
+            continue
+        A[dst, src]=1
+        try:
+            indegree[dst]+=1
+        except KeyError:
+            indegree[dst]=1
+
+        try:
+            outdegree[src]+=1
+        except KeyError:
+            indegree[src]=1
+
+    return A, dict, dict
